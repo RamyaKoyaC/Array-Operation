d2de4ce58168771d4d9652e343a9a1d309128d63
Ramya <ramyakrc10@gmail.com>
2018-03-28 18:53:49 -0500

    Array operations
    To find max value in Array

---
+++ Array Operations/Finding Max in an Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int[] marks = new int[] { 61, 67, 100, 45, 97 };
+
+            
+                int Highestmark = marks.Max();
+               
+                Console.WriteLine(Highestmark);
+                Console.ReadKey();
+
+            
+        }
+    }
+}
